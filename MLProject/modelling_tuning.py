d5f534f7aef989aa186c32d9bc1d06a4292d7c21
Allnput <Chyntyachll@gmail.com>
2025-12-14 17:46:29 +0700

    Update modelling_tuning.py

--- MLProject/modelling_tuning.py
+++ MLProject/modelling_tuning.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 import mlflow
 import mlflow.sklearn
-mlflow.set_tracking_uri("file:./mlruns")
-os.makedirs("mlruns", exist_ok=True)
+# mlflow.set_tracking_uri("file:./mlruns")
+# os.makedirs("mlruns", exist_ok=True)
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
